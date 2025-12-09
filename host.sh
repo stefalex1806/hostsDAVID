6214e62bc44be774d3e0f2a105777ee477752a84
RDS1208 <davidrosu1208@gmail.com>
2025-12-10 01:08:46 +0200

    Adaugat scriptul host.sh

---
+++ host.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat /etc/hosts | while read ip name rest; do
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [[ -n "$name" ]]; then
+        ip_nslookup=$(nslookup "$name" 2>/dev/null | grep -A 1 'Name:' | grep 'Address:' | awk '{print $2}' | tail -n 1)
+        if [[ -n "$ip_nslookup" ]] && [[ "$ip" != "$ip_nslookup" ]]; then
+            echo "Bogus IP for $name in /etc/hosts! Config: $ip, Resolved: $ip_nslookup"
+        fi
+    fi
+done
